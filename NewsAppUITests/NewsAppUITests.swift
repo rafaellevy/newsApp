bb7ca26507ec55190371259ea18110dae1c000ed
rafaellevy <srlevy2@gmail.com>
2021-08-20 22:56:53 -0700

    Project name changed

--- NewsAppUITests/NewsAppUITests.swift
+++ NewsAppUITests/NewsAppUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class GitFriendsUITests: XCTestCase {
+class NewsAppUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
